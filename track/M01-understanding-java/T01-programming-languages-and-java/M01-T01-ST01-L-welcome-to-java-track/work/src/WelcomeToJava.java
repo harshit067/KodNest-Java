5a21a18a51ac367aebd71817f9dfae89b9accbe0
harshit-kamboj380 <harshitkamboj380@gmail.com>
2026-07-30 18:52:57 +0530

    One more line added

--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
@@ -2,7 +2,8 @@
 public class WelcomeToJava {
 
     public static void main(String[] args) {
-        System.out.println("Welcome to java track");
+        System.out.println("Welcome to Java track");
         System.out.println("I already know how to build programming logic");
+        System.out.println("Now I am learning how to express that logic using Java.");
     }
 }
