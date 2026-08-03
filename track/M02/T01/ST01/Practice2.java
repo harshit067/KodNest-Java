9b5a3e972928f6a2d5f2f2f97ceaff31a0faba2d
harshit-kamboj380 <harshitkamboj380@gmail.com>
2026-08-03 16:28:06 +0530

    M02\T01\ST01

---
+++ track/M02/T01/ST01/Practice2.java
@@ -0,0 +1,11 @@
+
+public class Practice2 {
+
+    public static void main(String[] args) {
+        String learnerName = "Salman";
+        int dailyPracticeHours = 8;
+        System.err.println("Name: " + learnerName);
+        System.err.println("Daily Practice: " + dailyPracticeHours + " Hours");
+
+    }
+}
