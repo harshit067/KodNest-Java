5c830631ba085a07f00ff51c828ccc74abb9a7d0
harshit-kamboj380 <harshitkamboj380@gmail.com>
2026-08-04 19:01:54 +0530

    M02\T01\ST01

---
+++ track/M02/T01/ST01/Practice4.java
@@ -0,0 +1,18 @@
+
+class Practice4 {
+
+    public static void main(String[] args) {
+        int completedTopics = 17;
+        int totalTopics = 20;
+        int dailyLearningHours = 3;
+        int learningDays = 5;
+        int remainingTopics = totalTopics - completedTopics;
+        int weeklyLearningHours = dailyLearningHours * learningDays;
+        double progressPercentage = (double) completedTopics / totalTopics * 100;
+        System.out.println("Completed Topics: " + completedTopics);
+        System.out.println("Remaining Topics: " + remainingTopics);
+        System.out.println("Weekly Learning Hours: " + weeklyLearningHours);
+        System.out.println("Progress Percentage: " + progressPercentage);
+
+    }
+}
