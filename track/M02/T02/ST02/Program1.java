1f5e2eb20a1b215e6c547a05fba61e4d88a2ec6a
harshit-kamboj380 <harshitkamboj380@gmail.com>
2026-08-05 15:23:58 +0530

    M02\T02\ST02

---
+++ track/M02/T02/ST02/Program1.java
@@ -0,0 +1,12 @@
+
+public class Program1 {
+
+    public static void main(String[] args) {
+        int num = 10;
+        if (num > 99) {
+            System.out.println("num is greater");
+        } else {
+            System.out.println("num is not greater");
+        }
+    }
+}
