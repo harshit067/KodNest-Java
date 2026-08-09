3ad3937fcf96e10a3762dd3f89e0b55f4e144686
harshit-kamboj380 <harshitkamboj380@gmail.com>
2026-08-09 20:22:22 +0530

    M02\T03\ST03

---
+++ track/M02/T03/ST03/Program10.java
@@ -0,0 +1,15 @@
+
+public class Program10 {
+
+    public static void main(String[] args) {
+        int total = 0;
+        for (int i = 1; i <= 5; i++) {
+            if (i == 3) {
+                continue;
+            }
+            System.out.println("Number: " + i);
+            total += i;
+        }
+        System.out.println(total);
+    }
+}
