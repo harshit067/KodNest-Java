53a3a4af464aaa7583350bedafb65dd6c33397b0
harshit-kamboj380 <harshitkamboj380@gmail.com>
2026-08-11 14:50:57 +0530

    M02\T03\ST03

---
+++ track/M02/T03/ST03/Program18.java
@@ -0,0 +1,30 @@
+
+class Employee {
+
+    int id;
+    String name;
+
+    void work() {
+        System.out.println("Working");
+    }
+}
+
+public class Program18 {
+
+    public static void main(String[] args) {
+        Employee e1 = new Employee();
+        e1.id = 10;
+        e1.name = "Anu";
+        System.out.println("e1.id");
+        System.out.println("e1.name");
+        e1.work();
+        Employee e2;
+        e2 = e1;
+        e2.id = 111;
+        e2.name = "Raja";
+        System.out.println(e1.id);
+        System.out.println(e1.name);
+        System.out.println(e2.id);
+        System.out.println(e2.name);
+    }
+}
