175af100ca6c502f761802085ad1d67c8738e545
harshit-kamboj380 <harshitkamboj380@gmail.com>
2026-08-11 15:31:30 +0530

    M02\T05\ST05

---
+++ track/M02/T05/ST05/Program2.java
@@ -0,0 +1,12 @@
+
+class Program2 {
+
+    public static void main(String[] args) {
+        add(10, 5);
+    }
+
+    static void add(int a, int b) {
+        int res = a + b;
+        System.out.println(res);
+    }
+}
