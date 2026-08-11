8d9e3da855abd39dd7a53c195d81e8ed897bfdd8
harshit-kamboj380 <harshitkamboj380@gmail.com>
2026-08-11 15:34:20 +0530

    M02\T05\ST05

---
+++ track/M02/T05/ST05/Program3.java
@@ -0,0 +1,15 @@
+
+public class Program3 {
+
+    public static void main(String[] args) {
+        Robot rb = new Robot();
+        rb.speak();
+    }
+}
+
+class Robot {
+
+    void speak() {
+        System.out.println("Beep beep! java is my superpower!");
+    }
+}
