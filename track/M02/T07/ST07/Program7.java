54947ff09e43777cfd7420c85e7c340897f3a6be
harshit-kamboj380 <harshitkamboj380@gmail.com>
2026-08-18 14:55:49 +0530

    M02\T07\ST07

---
+++ track/M02/T07/ST07/Program7.java
@@ -0,0 +1,15 @@
+
+public class Program7 {
+
+    public static void main(String[] args) {
+        int[] stock = {4, 12, 2, 9, 5};
+        int reorderLevel = 5;
+        int reorderCount = 0;
+        for (int i = 0; i < stock.length; i++) {
+            if (stock[i] <= reorderLevel) {
+                reorderCount++;
+            }
+        }
+        System.out.println("Items to reorder: " + reorderCount);
+    }
+}
