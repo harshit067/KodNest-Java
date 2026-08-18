23ed638cecd40b194205d6a6fbe64af8203d5c37
harshit-kamboj380 <harshitkamboj380@gmail.com>
2026-08-18 15:56:20 +0530

    M02\T08\ST08

---
+++ track/M02/T08/ST08/Program2.java
@@ -0,0 +1,26 @@
+
+class ArraySearch {
+
+    void search(int arr[], int key) {
+        boolean found = false;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == key) {
+                System.out.println("Element found at index: " + i);
+                found = true;
+                break;
+            }
+        }
+        if (!found) {
+            System.out.println("Element not found");
+        }
+    }
+}
+
+public class Program2 {
+
+    public static void main(String[] args) {
+        int arr[] = {10, 20, 30, 40, 50};
+        ArraySearch as = new ArraySearch();
+        as.search(arr, 30);
+    }
+}
