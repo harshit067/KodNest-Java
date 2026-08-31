05a07744447cafa15b2ca44b8ced6eb624e6b95f
harshit-kamboj380 <harshitkamboj380@gmail.com>
2026-08-31 10:22:17 +0530

    M03\T02\ST02

---
+++ track/M03/Program1.java
@@ -0,0 +1,28 @@
+
+import java.util.Scanner;
+
+public class Program1 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of elements");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter the elements");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += arr[i];
+        }
+        double avg = (double) sum / n;
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] > avg) {
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
