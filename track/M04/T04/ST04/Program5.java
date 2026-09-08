9f2e4203c5e3024d37824b51f7bdfc655e5cc74d
harshit-kamboj380 <harshitkamboj380@gmail.com>
2026-09-08 19:21:44 +0530

    M04\T04\ST04

---
+++ track/M04/T04/ST04/Program5.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;
+
+public class Program5 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String firstText = scanner.nextLine();
+        String secondText = scanner.nextLine();
+
+        if (firstText.equals(secondText)) {
+            System.out.println("Exact match: true");
+        } else {
+            System.out.println("Exact match: false");
+        }
+
+        if (firstText.equalsIgnoreCase(secondText)) {
+            System.out.println("Ignore-case match: true");
+        } else {
+            System.out.println("Ignore-case match: false");
+        }
+    }
+}
