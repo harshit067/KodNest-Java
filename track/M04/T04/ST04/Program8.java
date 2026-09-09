e94c79b3c2b5a97b2387211fb21f8a144e8c38a3
harshit-kamboj380 <harshitkamboj380@gmail.com>
2026-09-09 19:12:51 +0530

    M04\T04\ST04

---
+++ track/M04/T04/ST04/Program8.java
@@ -0,0 +1,15 @@
+
+public class Program8 {
+
+    public static void main(String[] args) {
+        String s1 = "Raja";
+        String s2 = "Rani";
+        String s3 = s1 + s2;
+        System.out.println(s3);
+
+        String s4 = new String("Tanu");
+        String s5 = new String("Shanu");
+        String s6 = s4.concat(s5).concat("Rama");
+        System.out.println(s6);
+    }
+}
